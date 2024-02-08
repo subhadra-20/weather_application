bf175bde20e13875f9fb94e935976c9d7bb570a9
subhadra-20 <sahoosubhadra302@gmail.com>
2024-02-08 10:19:37 +0530

    adding some new files

--- weatherapp/src/App.js
+++ weatherapp/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from './Common/Header'
+import Footer from './Common/Footer'
+import Navbar from './Common/Navbar'
 
 function App() {
   return (
     <div>
-      
+      <Header/>
+      <Navbar/>
+      <Footer/>
     </div>
   )
 }
